a1d73e1ac58c37991120cc93815b6d70c0ce3b80
Aditya Tummala <adi.tummala@gmail.com>
2024-03-01 13:27:06 -0800

    moved to folders.

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import json
 import logging
-from video import extract_frames
+from connect4.video import extract_frames
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
 
 print(extract_frames('c4cv.mov'))
 
 
 def lambda_handler(event, context):
     # Assuming the video file is stored in /tmp directory within the Lambda environment
-    video_path = 'c4cv.mov'
+    video_path = '/tmp/c4cv.mov'
     output_strings = extract_frames(video_path)
 
     # dummy output
@@ -28,3 +28,4 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": "\n".join(output_strings)
     }
+
